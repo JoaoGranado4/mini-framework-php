25480264481b1d720ce26607aca287ac2287bec1
Joao Granado <joao.granado4@gmail.com>
2021-08-12 16:48:20 -0300

    Add novos controles

--- app/config/router.php
+++ app/config/router.php
@@ -1,9 +1,15 @@
 <?php
 
-$this->get('/home/', function () {
+$this->get('/', function () {
+    echo "Home!!!";
+});
+
+$this->get('/home', function () {
     echo "Estou na Home";
 });
 
 $this->get('/about/test', function () {
     echo "Estou na about test";
 });
+
+$this->get('/categoria', 'MyController@method');
