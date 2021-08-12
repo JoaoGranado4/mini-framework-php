ec6a85dc4ebccc33903d6b83d1a40f824f9e282a
Joao Granado <joao.granado4@gmail.com>
2021-08-12 00:45:11 -0300

    implementando sistema de rotas

---
+++ app/config/router.php
@@ -0,0 +1,9 @@
+<?php
+
+$this->get('/home/', function () {
+    echo "Estou na Home";
+});
+
+$this->get('/about/test', function () {
+    echo "Estou na about test";
+});
--- public/index.php
+++ public/index.php
@@ -1,8 +1,9 @@
 <?php
 
+use app\core\RouterCore;
+
 require_once('../vendor/autoload.php');
+require_once('../app/config/config.php');
 require_once('../app/functions/functions.php');
 
-use app\controller\TesteController;
-(new \app\core\RouterCore());
-$controller = new TesteController();
+(new RouterCore());
